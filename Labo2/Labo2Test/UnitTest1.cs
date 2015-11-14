f461470d9f9cc4e14500d4f83cb256aea2e6c170
Quentin051294 <quentin_nico@hotmail.com>
2015-11-14 11:07:08 +0100

    test ok

---
+++ Labo2/Labo2Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibrairieLabo2;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Labo2Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InsertionFonctionnelle()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<CompanyContext>());
+            Customer quentin = new Customer();
+            quentin.City = "Moustier";
+            CompanyContext context = new CompanyContext(@"Data Source=vm-sql.iesn.be\stu3ig;Initial Catalog=DBIG3B3;User ID=IG3B3;Password=pwUserdbp");
+            context.Customers.Add(quentin);
+            context.SaveChanges();
+            TestMethod2();
+        }
+
+        public void TestMethod2()
+        {
+            CompanyContext context = new CompanyContext(@"Data Source=vm-sql.iesn.be\stu3ig;Initial Catalog=DBIG3B3;User ID=IG3B3;Password=pwUserdbp");
+            Assert.IsTrue(context.Customers.Count() >= 1);
+        }
+    }
+}
---
+++ Labo2/LibrairieLabo2/CompanyContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrairieLabo2
+{
+   public class CompanyContext : DbContext
+    {
+        public CompanyContext(String ConnectionString) : base(ConnectionString)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
